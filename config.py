a91aa4f38f910fd747525721b64ade181f2dd074
Coi SR <150426205+nossah56@users.noreply.github.com>
2024-01-14 21:39:01 +0200

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ OWNER_ID = list(
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/ma7amad2000/AHMED")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/nossah56/go.git")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
